8ef43e211623dbeb1a0288997d2c05523c146aab
andhikawilby <andhikawilby@gmail.comm>
2022-10-02 20:55:12 -0700

    swagger_1

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, jsonify, request #import objects from the Flask model
+app = Flask(__name__) #define app using Flask
+from flask_swagger_ui import get_swaggerui_blueprint
+
+
+# flask swagger configs
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Todo List API"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+
+
+
+languages = [{'name' : 'JavaScript'}, {'name' : 'Python'}, {'name' : 'Ruby'}]
+
+@app.route('/', methods=['GET'])
+def test():
+	return jsonify({'message' : 'It works!'})
+
+@app.route('/lang', methods=['GET'])
+def returnAll():
+	return jsonify({'languages' : languages})
+
+@app.route('/lang/<string:name>', methods=['GET'])
+def returnOne(name):
+	langs = [language for language in languages if language['name'] == name]
+	return jsonify({'language' : langs[0]})
+
+@app.route('/lang', methods=['POST'])
+def addOne():
+	language = {'name' : request.json['name']}
+
+	languages.append(language)
+	return jsonify({'languages' : languages})
+
+@app.route('/lang/<string:name>', methods=['PUT'])
+def editOne(name):
+	langs = [language for language in languages if language['name'] == name]
+	langs[0]['name'] = request.json['name']
+	return jsonify({'language' : langs[0]})
+
+@app.route('/lang/<string:name>', methods=['DELETE'])
+def removeOne(name):
+	lang = [language for language in languages if language['name'] == name]
+	languages.remove(lang[0])
+	return jsonify({'languages' : languages})
+
+if __name__ == '__main__':
+	app.run(debug=True, port=8080) #run app on port 8080 in debug mode
